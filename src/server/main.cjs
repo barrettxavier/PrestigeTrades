fba1e8bf1289d55eb1d9ae9a8190b4d9801cde76
barrettxavier <barrettxavier@yahoo.com>
2024-03-04 12:53:52 -0500

    update to deploy

--- src/server/main.cjs
+++ src/server/main.cjs
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const ViteExpress = require("vite-express");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 
@@ -9,6 +10,9 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/dist/", express.static(path.join(__dirname, "../dist")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 // Routes
 app.use("/api", require("./api/index.cjs"));
